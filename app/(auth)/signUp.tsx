85b816f67720e639dc12e6da05b875399f1057a8
shubhamraj82 <raj.shubh8216@gmail.com>
2025-08-01 01:26:02 +0530

    sign up done

--- app/(auth)/signUp.tsx
+++ app/(auth)/signUp.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import SignUpScreen from '@/components/Screens/auth/SignUp'
+import SignUpScreen from '@/components/Screens/auth/CustomerSignUp'
 
 const signUp = () => {
   return (
