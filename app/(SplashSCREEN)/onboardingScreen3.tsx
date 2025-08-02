ebd458541523803c46df7c573fd023e86de409fd
shubhamraj82 <raj.shubh8216@gmail.com>
2025-08-02 11:40:58 +0530

    deleted onboard design

--- app/(SplashSCREEN)/onboardingScreen3.tsx
+++ app/(SplashSCREEN)/onboardingScreen3.tsx
@@ -1,11 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import OnboardingScreen from '@/components/Screens/SplashScreen/OnboardingScreen3'
 
 const onboardingScreen3 = () => {
   return (
     <View>
-    <OnboardingScreen/>
+   
     </View>
   )
 }
